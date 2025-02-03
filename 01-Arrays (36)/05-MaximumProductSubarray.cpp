a472fcb97c02654ae016764fb42ea10e30ef5e96
praveen <praveensrinivasan521@gmail.com>
2025-02-03 22:45:45 +0530

    Array, DP

--- 01-Arrays (36)/05-MaximumProductSubarray.cpp
+++ 01-Arrays (36)/05-MaximumProductSubarray.cpp
@@ -1,26 +1,24 @@
-class Solution {
-public:
-    int maxProduct(vector<int>& nums) {
-        int maxi = INT_MIN;
-        int prod=1;
+#include <bits/stdc++.h> 
+int maximumProduct(vector<int> &nums, int n)
+{
+	int maxi = INT_MIN;
+	int prod=1;
 
-        for(int i=0;i<nums.size();i++)
-        {
-          prod*=nums[i];
-          maxi=max(prod,maxi);
-          if(prod==0)
-           prod=1;
-        }
-        
-        prod=1;
-        for(int i=nums.size()-1;i>=0;i--)
-        {
-          prod*=nums[i];
+	for(int i=0;i<n;i++)
+	{
+		prod*=nums[i];
+		maxi=max(prod,maxi);
+		if(prod==0)
+		prod=1;
+	}
+	prod=1;
+	for(int i=n-1;i>=0;i--)
+	{
+		prod*=nums[i];
 
-          maxi=max(prod,maxi);
-          if(prod==0)
-           prod=1;
-        }
-        return maxi;
-    }
-};
+		maxi=max(prod,maxi);
+		if(prod==0)
+		prod=1;
+	}
+	return maxi;
+}
