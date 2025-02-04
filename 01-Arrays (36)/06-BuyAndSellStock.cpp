3f0ff012d9fd91b710fef1a008a65455b90609fc
praveen <praveensrinivasan521@gmail.com>
2025-02-04 23:29:33 +0530

    Array, DP

---
+++ 01-Arrays (36)/06-BuyAndSellStock.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h> 
+int maximumProfit(vector<int> &prices){
+    int buyPrice = prices[0];
+    int profit = 0;
+
+    for (int i = 1; i < prices.size(); i++) {
+        if (buyPrice > prices[i]) {
+            buyPrice = prices[i];
+        }
+
+        profit = max(profit, prices[i] - buyPrice);
+    }
+
+    return profit;  
+}
